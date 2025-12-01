e068fb018a1007d569141b79bd04449705c64366
Satyam Gangwar <gangwar.satyam01@gmail.com>
2025-12-01 12:52:55 +0530

    solved envi variables problem

--- backend/src/main/java/com/gangwarsatyam/sharenest/service/CloudinaryService.java
+++ backend/src/main/java/com/gangwarsatyam/sharenest/service/CloudinaryService.java
@@ -9,7 +9,9 @@ import java.io.IOException;
 import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 
+@ConditionalOnProperty(name = "cloudinary.enabled", havingValue = "true")
 @Service
 public class CloudinaryService {
 
