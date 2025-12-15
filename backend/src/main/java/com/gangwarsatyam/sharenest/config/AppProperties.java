c05b6df12a6d166af4665c03e7e26de2886bb3f5
Satyam Gangwar <gangwar.satyam01@gmail.com>
2025-12-15 18:02:50 +0530

    updated jwtProvider

--- backend/src/main/java/com/gangwarsatyam/sharenest/config/AppProperties.java
+++ backend/src/main/java/com/gangwarsatyam/sharenest/config/AppProperties.java
@@ -1,12 +1,11 @@
 package com.gangwarsatyam.sharenest.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 import java.util.List;
 
-@Component
 @ConfigurationProperties(prefix = "app")
 public class AppProperties {
+
     private List<String> frontendUrl;
 
     public List<String> getFrontendUrl() {
